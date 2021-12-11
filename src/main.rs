3a34713bba6a95e3a05092defcb1daa2275984cd
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-12-11 09:28:39 +0100

    feat (main): Generate first image.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = (256, 256);
+
+    println!("P3\n{} {}\n255\n", width, height);
+
+    for j in (0..height).rev() {
+        for i in 0..width {
+            let r = i as f64 / (width - 1) as f64;
+            let g = j as f64 / (height - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as usize;
+            let ig = (255.999 * g) as usize;
+            let ib = (255.999 * b) as usize;
+
+            println!("{} {} {}\n", ir, ig, ib);
+        }
+    }
 }
