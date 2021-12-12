73c0985d6cbcdda7c8f9942727a1d47e227b733b
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-12-12 18:51:15 +0100

    feat (main): Add normal mapped sphere.

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,10 @@ fn main() {
 }
 
 fn ray_color(ray: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let N = unit_vector(ray.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
     }
     let unit_direction = unit_vector(ray.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
@@ -58,11 +60,15 @@ fn colorize(color: Color) -> String {
     )
 }
 
-fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> f64 {
     let oc = ray.origin() - center;
     let a = dot(ray.direction(), ray.direction());
     let b = 2.0 * dot(oc, ray.direction());
     let c = dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt() ) / (2.0 * a)
+    }
 }
